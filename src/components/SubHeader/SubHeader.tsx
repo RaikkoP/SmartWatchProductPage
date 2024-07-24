995e2677aadbd5a232a9c54c19aabbd3ba3fe381
RaikkoP <raikko.prants@gmail.com>
2024-07-25 00:22:35 +0300

    [SWP/Header] Added subheader left banner, fixed image location of subheader and made menus look like original

--- src/components/SubHeader/SubHeader.tsx
+++ src/components/SubHeader/SubHeader.tsx
@@ -1,23 +1,44 @@
+import './SubHeader.scss';
 interface HeaderProps {
-    subHeaderItems: {name: string; link: string; }[];
+    subHeaderItems: {name: string; link: string; }[][];
     subHeaderTitles: string[];
 }
 
-export default function SubHeader({subHeaderItems = [], subHeaderTitles = []}: HeaderProps) {
+export default function SubHeader({subHeaderItems = [[]], subHeaderTitles = []}: HeaderProps) {
     return (
-            <div>
-                {subHeaderTitles?.map((item, index) => (
-                    <div>
-                        <h2 key={index}>{item}</h2>
-                        <ul className='header__subheader'>
-                            {subHeaderItems?.map((item, index) => (
-                                <li className='header__subheader--item' key={index}>
-                                    <a href={item.link}>{item.name}</a>
-                                </li>
-                            ))}
-                        </ul>
+            <div className='subHeader'>
+                <div className='subHeader--filler'>
+                    <div className='subHeader--filler-inner'>
+                        <div className='shapes shapes--left'>
+                            <div className='shapes__circle'/>
+                            <div className='shapes__triangle-down'/>
+
+                        </div>
+                        <h2 className='subHeader--filler-name'>Xiaomi</h2>
+                        <div className='shapes shapes--right'>
+                            <div className='shapes__circle'/>
+                            <div className='shapes__triangle-down'/>
+
+                        </div>
                     </div>
-                ))}
+                </div>
+                <div className='subHeader--content'>
+                    <img src='../../images/phone.png' className='subHeader--image' alt='phone'/>
+                    <div className='subHeader--content-lists'>
+                        {subHeaderTitles?.map((item, index) => (
+                            <div className='subHeader__section'>
+                                <h2 className='subHeader__section--title' key={index}>{item}</h2>
+                                <ul className='subHeader__section--list'>
+                                    {subHeaderItems[index]?.map((item, index) => (
+                                        <li className='subHeader__section--list-item' key={index}>
+                                            <a href={item.link}>{item.name}</a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
     )
 }
--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -1,14 +1,17 @@
 import './Header.scss';
 import Icon from '../../components/Icon/Icon';
 import SubHeader from '../../components/SubHeader/SubHeader';
+import { useState } from 'react';
 interface HeaderProps {
     hasSubHeader? : boolean;
-    subHeaderItems?: {name: string; link: string; }[];
+    subHeaderItems?: {name: string; link: string; }[][];
     subHeaderTitles?: string[];
 }
 
-export default function Header({hasSubHeader = false, subHeaderItems = [], subHeaderTitles = []}: HeaderProps) {
+export default function Header({hasSubHeader = false, subHeaderItems = [[]], subHeaderTitles = []}: HeaderProps) {
+
     return (
+        <>
         <header className='header'>
             <Icon icon='xiaomi.svg' alt='Company Logo' size='icon__lg'/>
             <ul className='header__list'>
@@ -17,9 +20,10 @@ export default function Header({hasSubHeader = false, subHeaderItems = [], subHe
                 <li className='header__list--item'><a href='#'>About</a></li>
                 <li className='header__list--item'><a href='#'>Support</a></li>
             </ul>
-            {hasSubHeader &&
+        </header>
+        {hasSubHeader &&
                 <SubHeader subHeaderItems={subHeaderItems} subHeaderTitles={subHeaderTitles} />
             }
-        </header>
+        </>
     )
 }
